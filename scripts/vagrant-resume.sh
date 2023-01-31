9549c393b6d04eff5dc21f3e3355dbc9caf2a82a
alfonso.catanzaro <alfonso.catanzaro@wolterskluwer.com>
2023-01-31 21:44:00 +0100

    vagrant scripts

---
+++ scripts/vagrant-resume.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+vagrant resume node1 &>/dev/null &
+vagrant resume node2 &>/dev/null &
+vagrant resume node3 &>/dev/null &
+vagrant resume node4 &>/dev/null &
+vagrant resume node5 &>/dev/null &
+vagrant resume node6 &>/dev/null &
+vagrant resume node7 &>/dev/null &
+vagrant resume node8 &>/dev/null &
+
+jobs
---
+++ scripts/vagrant-suspend.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+vagrant suspend node1 &>/dev/null &
+vagrant suspend node2 &>/dev/null &
+vagrant suspend node3 &>/dev/null &
+vagrant suspend node4 &>/dev/null &
+vagrant suspend node5 &>/dev/null &
+vagrant suspend node6 &>/dev/null &
+vagrant suspend node7 &>/dev/null &
+vagrant suspend node8 &>/dev/null &
+
+jobs
