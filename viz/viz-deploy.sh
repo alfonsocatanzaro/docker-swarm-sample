662a15a056e3a159c0b5077377403ed40fa0c986
alfonso.catanzaro <alfonso.catanzaro@wolterskluwer.com>
2023-02-05 22:08:34 +0100

    viz as service

--- viz/viz-deploy.sh
+++ viz/viz-deploy.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-docker --context node1 stack deploy --compose-file viz-stack.yml viz
+docker \
+--context node1 \
+service create \
+--name=viz \
+--publish=8080:8080/tcp \
+--constraint=node.role==manager \
+--mount=type=bind,src=//var/run/docker.sock,dst=/var/run/docker.sock \
+weshigbee/swarmgs2-viz
